c192b06ec5164cb22862bd94bfc19cf57ab375fd
Simon <simon@sscott-laptop.eecs.berkeley.edu>
2012-04-12 15:53:48 -0700

    Added top-level matlab files.

---
+++ matlab/display_3d.m
@@ -0,0 +1,9 @@
+function display_3d(image)
+% Makes a 3D plot of the provided image.
+% 'image' is a 3D matrix
+% A "dot" is plotted at each non-zero location in the 3D matrix.
+% The colour of the dot is determined by the value at that location.
+
+% To write
+
+end
---
+++ matlab/main.m
@@ -0,0 +1,3 @@
+s = generate_radar_data();
+image = process_radar_data(s);   % change to Patrick's function's name
+display_3d(image);
