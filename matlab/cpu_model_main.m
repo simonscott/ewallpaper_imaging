7ce56a0949c3fa3a99dce5c4326e03ff035c5acc
simonscott <simonscott@berkeley.edu>
2012-04-30 17:00:00 -0700

    Added Hopper performance measurements to Matlab computation model.

--- matlab/cpu_model_main.m
+++ matlab/cpu_model_main.m
@@ -13,6 +13,7 @@ Nx = 128;
 Ny = 128;
 Nf = 256;
 
+% Compute model
 [total_time, comp_time, comm_time] = cpu_model(latency, bandwidth, ...
                        f_add_cycles, f_mult_cycles, ... 
                        clock_speed, mem_cycles, Nx, Ny, Nf);
@@ -20,5 +21,13 @@ Nf = 256;
 cpu_load = comp_time / total_time;
 frame_rate = (1/total_time);
 
+% Calculate computational time based on Hopper measurements
+hopper_num_flops = 29252;
+actual_comp_time = hopper_num_flops * (f_add_cycles + f_mult_cycles)/2 ...
+                    / clock_speed;
+
+% Display results
 fprintf('Total time = %f, framerate = %f, CPU load = %f\n', ...
             total_time, frame_rate, cpu_load);
+fprintf('Modeled computation time = %f, actual computation time = %f\n', ...
+            comp_time, actual_comp_time);
